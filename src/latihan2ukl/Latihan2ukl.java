75e71d37073f2e03ac213d9cba54d02d91c24a51
ZAID6470 <muhammad_zaidan_29rpl@student.smktellkom-mlg.sch.id>
2021-07-30 09:15:22 +0700

    UKL

---
+++ src/latihan2ukl/Latihan2ukl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2ukl;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Latihan2ukl {
+
+    
+    public static void main(String[] args) {
+        int a = 4;
+
+    
+    for (int b = 1; b <= a; ++b) {
+      for (int c = 1; c <= b; ++c) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+    }
+    
+}
---
+++ src/latihan2ukl2/latihan2ukl2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2ukl2;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class latihan2ukl2 {
+    public static void main(String[] args) {
+        int a = 5;
+
+    
+    for (int b = 1; b <= a; ++b) {
+        System.out.println("");
+      
+        for (int c = b; c <= a; ++c) {
+            System.out.print("*");
+      }
+      
+    }
+    
+    }
+}
